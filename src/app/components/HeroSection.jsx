3478aa123e869dc904e6cffe57cb5c363dd7ddb1
EmanuelDavid <emadg07.ed@gmail.com>
2024-06-10 21:24:50 -0300

    Add hero image and HeroSection component, update page.js

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,42 @@
+// 11min
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+          <div className="col-span-7 place-self-center">
+              <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hola, Soy Emanuel David Garcia</h1>
+              <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse officiis
+                nulla labore itaque nesciunt dolorum commodi voluptatum? Accusamus unde
+                repellat obcaecati qui dignissimos inventore eveniet nesciunt tenetur
+                accusantium, hic culpa.
+              </p>
+              <div>
+                <button className="px-6 py-3 rounded-full mr-4">
+                    Sobre mi
+                </button>
+                <button className="px-6 py-3 rounded-full">
+                    Descargar CV
+                </button>
+              </div>
+          </div>
+          <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image 
+            src="/images/hero-image.png"
+            alt = "hero image"
+            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            width = {300}
+            height = {300}
+            />
+            </div>
+          </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
