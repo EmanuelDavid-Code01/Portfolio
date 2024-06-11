0bd5cccc5d37d28f5be7d8af3aa15f4627a193a9
EmanuelDavid <emadg07.ed@gmail.com>
2024-06-11 01:14:30 -0300

    CSS styles text

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             Hola, Soy Emanuel David Garcia
           </h1>
           <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
